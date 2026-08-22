a12ab6f5f9722a97cc072a9dbe2f258e79128495
Phoebe <phoebe@Phoebes-MAC.local>
2026-08-21 22:24:02 -0500

    Scaffold PM app, wire up all 6 Dataverse data sources

---
+++ .power/schemas/appschemas/dataSourcesInfo.ts
@@ -0,0 +1,50 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * This file is auto-generated. Do not modify it manually.
+ * Changes to this file may be overwritten.
+ */
+
+export const dataSourcesInfo = {
+  "cre2b_feedbacks": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cre2b_feedbackid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cre2b_personnels": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cre2b_personnelid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cre2b_servicecenterproximities": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cre2b_servicecenterproximityid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cre2b_servicecenters": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cre2b_servicecenterid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cre2b_workorders": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cre2b_workorderid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cre2b_workorderstatushistories": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cre2b_workorderstatushistoryid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  }
+};
